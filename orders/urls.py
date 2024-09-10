adb774079b627bec5d980c87aefca0737cb65f8f
Deepak Hared <dhared@deqode.com>
2024-09-10 14:59:47 +0530

    minor fixes

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 
 from orders import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", views.index, name="index"),
     path("create-order/", views.create_order, name="create_order"),
 ]
